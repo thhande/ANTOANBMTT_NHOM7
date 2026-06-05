5eae4bb3c3a2f22a438cbaaf917c6ad686f56928
thhande <135616907+thhande@users.noreply.github.com>
2026-06-05 08:34:10 +0700

    Update Program.cs

--- Tuan3/Phạm Hoàng Minh/Program.cs
+++ Tuan3/Phạm Hoàng Minh/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            var elgamal = new ElGamal();
+
+            // p must be prime, g a primitive root of p
+            Key key = elgamal.GenerateKey(p: 23, g: 5);
+            Console.WriteLine($"Private key x={key.X}, Public key y={key.Y}");
+
+            string message = "Hello, ElGamal!";
+            Signature sig = elgamal.Sign(message, key);
+            Console.WriteLine($"Signature: {sig}");
+
+            bool valid = elgamal.Verify(message, sig, key);
+            Console.WriteLine($"Valid: {valid}");           // True
+
+            bool tampered = elgamal.Verify("Tampered!", sig, key);
+            Console.WriteLine($"Tampered valid: {tampered}"); // False
         }
     }
 }
