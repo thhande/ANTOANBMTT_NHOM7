1f97d11498d841447550e5ba0781eef80d58dbbe
thhande <135616907+thhande@users.noreply.github.com>
2026-06-05 07:57:00 +0700

    added Minh's work on programing
    added elgamal class and example console

---
+++ Tuan3/Phạm Hoàng Minh/Elgama.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Key
+    {
+        private int privateKey;
+        public int[] PublicKey = new int[3];
+    }
+
+    class Signature
+    {
+        public int R { get; private set; }
+        public int S { get; private set; }
+
+        public Signature(int r,int s)
+        {
+            this.R = r;
+            this.S = s;
+        }
+    }
+
+
+    class Elgama
+    {
+        public void GenerateKey(int p,int g)
+        {
+
+        }
+
+        public int[] Sign(string message,int x)
+        {
+            return new int[]{ };
+        }
+
+        public bool Verify(string message,Signature signature,int y)
+        {
+            return true;
+        }
+
+
+
+
+
+
+
+    }
+}
